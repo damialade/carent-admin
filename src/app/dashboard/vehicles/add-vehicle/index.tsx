3e7f4f74efb3d2a75af4c0b352a65cc7ab4156b8
damialade <omoladedamilola1989@gmail.com>
2024-05-28 22:56:41 +0100

    filtering vehicles

---
+++ src/app/dashboard/vehicles/add-vehicle/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { Fragment } from "react";
+import WithSideBar from "@/components/Layout/WithSideBar";
+
+const AddVehicle = () => {
+  return (
+    <Fragment>
+      <WithSideBar>
+        <div>Add Vehicle</div>
+      </WithSideBar>
+    </Fragment>
+  );
+};
+
+export default AddVehicle;
